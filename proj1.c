4a40093e9d88d66190c906ee434d895bbd79e3fc
xsigmar <vojta.marek@gmail.com>
2017-10-18 19:57:37 +0200

    swap using integers

--- proj1.c
+++ proj1.c
@@ -8,9 +8,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(int argc, char const *argv[])
+/*předavame funkci */
+void vymen (int *ux, int *uy)
 {
-  printf("Hello, project nr. 1\n\n");
+    int pomoc;
+    pomoc = *ux;
+    *ux = *uy;
+    *uy = pomoc;
+
+}
+
+int main(void)
+{
+  int x, y;
+  printf("Zadejte dvě čísla:\n");
+  scanf("%d %d",&x, &y);
+  printf("Vaše čísla jsou\nx: %d\ny: %d\n", x, y);
+  printf("Chcete hodnoty proměnných přehodit? (y/n)\n");
+  char z;
+  while ((z =getchar()) != EOF)
+  {
+    if (z == 'y'|| z == 'Y')
+    {
+      vymen(&x, &y); //musíme funkci předat adresy prom. jinak funkce pouze vymeni kopie argumentů, ale prom v mainu zůstanou nezmenene
+      printf("Vámi vyměněné proměnné jsou nyní:\n");
+      printf("x: %d\ny: %d\n", x, y);
+      return 0;
+    }
+    else if (z == 'n' || z == 'N')
+    {
+      printf ("\nKonec programu\n");
+      return 0;
+    }
+  }
+
   return 0;
 }
